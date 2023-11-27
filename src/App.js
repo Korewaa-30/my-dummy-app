f3eaa8efac7ccbe1abafb85b9c8e3437cbc97277
Korewaa-30 <dukersarah30@gmail.com>
2023-11-27 10:57:33 +0000

    created a react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>My first test with React in VSCode</h1>
+				<p>This hasn't been easy at all</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
