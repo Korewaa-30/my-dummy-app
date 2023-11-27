061bc367212ef84593b180ea36963f51b22b4817
Korewaa-30 <dukersarah30@gmail.com>
2023-11-27 11:21:00 +0000

    changes restored

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
+import React, { useState } from "react";
 import axios from "axios";
 
 import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>My first test with React in VSCode</h1>
-				<p>This hasn't been easy at all</p>
-			</header>
-		</div>
-	);
-}
+export default function App() {
+	let [city, changeCity] = useState("");
+	let [weather, changeWeather] = useState({});
+	const [refreshed, changeRefreshed] = useState(false);
 
-export default App;
+	function currentWeather(response) {
+		changeRefreshed(true);
+		changeWeather({
+			city: response.data.city,
+			temperature: response.data.temperature.current,
+			wind: response.data.wind.speed,
+			humidity: response.data.temperature.humidity,
+			icon: response.data.condition.icon_url,
+			description: response.data.condition.description,
+		});
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		let apiKey = "8942b6bt940fbcoac33ad8a55c33f639";
+		let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+		axios.get(apiUrl).then(currentWeather);
+	}
+
+	function updateCity(event) {
+		changeCity(event.target.value);
+	}
+
+	let form = (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="search"
+				placeholder="Enter a city.."
+				onChange={updateCity}
+			/>
+			<button type="Submit">Search</button>
+		</form>
+	);
+
+	if (refreshed) {
+		return (
+			<div className="App">
+				<h1>My Weather App</h1>
+				{form}
+				<ul>
+					<li>Temperature: {Math.round(weather.temperature)}°C</li>
+					<li>Description: {weather.description}</li>
+					<li>Humidity: {weather.humidity}%</li>
+					<li>Wind: {weather.wind}km/h</li>
+					<li>
+						<img src={weather.icon} />
+					</li>
+				</ul>
+			</div>
+		);
+	} else {
+		return (
+			<div className="App">
+				<h1>My Weather App</h1>
+				{form}
+			</div>
+		);
+	}
+}
